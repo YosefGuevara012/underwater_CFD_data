5fb29ecf80df4f7af24df6977721b3cc756c4033
yosef <yosef.guevara012@hotmail.com>
2024-05-15 12:18:31 +0200

    3D squate shape added

---
+++ voxel_grids/Figures/3D/from_txt_to_voxel.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+# Definir dos matrices de ejemplo
+import numpy as np
+
+# Definir dos matrices de ejemplo
+finn_layer = np.array(
+                [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+                 [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+print(f"finn layer shape: {finn_layer.shape}")
+
+side_layer = np.array(
+                [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+
+print(f"Side layer shape: {side_layer.shape}")
+
+
+zeros_matrix = np.zeros((27,11))
+
+
+# Función para rotar una matriz multidimensional en 90 grados en el plano XY
+def rotate_90_XY(matrix):
+    return np.rot90(matrix, axes=(0, 1))
+
+# Función para rotar una matriz multidimensional en 90 grados en el plano XZ
+def rotate_90_XZ(matrix):
+    return np.rot90(matrix, axes=(0, 2))
+
+# Función para rotar una matriz multidimensional en 90 grados en el plano YZ
+def rotate_90_YZ(matrix):
+    return np.rot90(matrix, axes=(1, 2))
+
+# Función para rotar una matriz multidimensional en 180 grados en el plano XY
+def rotate_180_XY(matrix):
+    return np.rot90(matrix, k=2, axes=(0, 1))
+
+# Función para rotar una matriz multidimensional en 180 grados en el plano XZ
+def rotate_180_XZ(matrix):
+    return np.rot90(matrix, k=2, axes=(0, 2))
+
+# Función para rotar una matriz multidimensional en 180 grados en el plano YZ
+def rotate_180_YZ(matrix):
+    return np.rot90(matrix, k=2, axes=(1, 2))
+
+
+n = 7
+stacked_matrices = np.repeat(zeros_matrix[np.newaxis, ...], n, axis=0)
+
+# final matrix to voxel
+result_matrix = np.stack((zeros_matrix,zeros_matrix,zeros_matrix,zeros_matrix,
+                          side_layer,finn_layer,side_layer,
+                          zeros_matrix,zeros_matrix,zeros_matrix,zeros_matrix), axis=0)
+
+
+# Print Final Matrix shape
+print("Matriz resultante:")
+print(result_matrix.shape)
+
+result_matrix = rotate_90_YZ(rotate_90_XY(rotate_90_XZ(result_matrix)))
+
+
+print(result_matrix.shape)
+
+obj_name = "AUV"
+
+np.save(f"voxel_grid_{obj_name}.npy",result_matrix)
+
+
+
+
